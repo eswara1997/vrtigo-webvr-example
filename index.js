f0fc1485c8da36e7045b96efed08187d29712e0f
Eswar Anandapadmanaban <Eswara@mit.edu>
2016-07-25 15:20:18 -0700

    fixing aframe integration

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import {addUserID, addAppID, addEvent, setPoseFrequency, setRenderFrequency, setBatteryFrequency} from 'vrtigo-analytics';
 
-console.log(setRenderFrequency);
-console.log(setPoseFrequency);
-console.log(setBatteryFrequency);
+ 
