707c0e3809c5d64b14a6a1bcfacb83d688aaa3a8
Eswar Anandapadmanaban <Eswara@mit.edu>
2016-07-25 17:01:55 -0700

    added function examples and fixed webpack bundle.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import {addUserID, addAppID, addEvent, setPoseFrequency, setRenderFrequency, setBatteryFrequency} from 'vrtigo-analytics';
+import {addUserId, setAppId, addEvent, setPoseFrequency, setRenderFrequency, setBatteryFrequency, setSampler} from 'vrtigo-analytics';
 
- 
+ var user = addUserId('user55@oculus.com');
+ var app = setAppId("0c680a1e-8a32-46d5-ab22-087d6ea8d1cf");
+ var setSample = setSampler(true);
