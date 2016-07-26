2d20c486a080699abde4b4cec82fe8515b0ec48d
Eswar Anandapadmanaban <Eswara@mit.edu>
2016-07-26 09:49:44 -0700

    fixing setUserID and adding README

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {addUserId, setAppId, addEvent, setPoseFrequency, setRenderFrequency, setBatteryFrequency, setSampler} from 'vrtigo-analytics';
+import {setUserId, setAppId, addEvent, setPoseFrequency, setRenderFrequency, setBatteryFrequency, setSampler} from 'vrtigo-analytics';
 
- var user = addUserId('user55@oculus.com');
+ var user = setUserId('user55@oculus.com');
  var app = setAppId("0c680a1e-8a32-46d5-ab22-087d6ea8d1cf");
  var setSample = setSampler(true);
